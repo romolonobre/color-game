fe0eb7ad1ecb738857fe1280a32508a40837581c
romolonobre <romoloferreira@icloud.com>
2024-09-16 22:05:43 +0100

    create ColorGameScreen

---
+++ lib/ui/color_game_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import '../controller/color_controller.dart';
+import 'custom_text.dart';
+
+class ColorGameScreen extends StatefulWidget {
+  const ColorGameScreen({super.key});
+
+  @override
+  State<ColorGameScreen> createState() => _ColorGameScreenState();
+}
+
+class _ColorGameScreenState extends State<ColorGameScreen> {
+  final ColorController controller = ColorController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: controller.currentColor,
+      builder: (_, currentColor, __) {
+        bool isColorBlack = currentColor == Colors.black;
+        bool isColorWhite = currentColor == Colors.white;
+
+        return Material(
+          child: InkWell(
+            onTap: () => controller.currentColor.value = controller.generateColor(),
+            child: Material(
+              color: currentColor,
+              child: Stack(
+                children: [
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(
+                          "Hello there",
+                          color: isColorBlack ? Colors.white : Colors.black,
+                        ),
+                        CustomText(
+                          "Tap me!!",
+                          fontSize: 15,
+                          color: isColorBlack ? Colors.white : Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: ElevatedButton(
+                      child: CustomText(
+                        "Don't tap me :)",
+                        fontSize: 20,
+                        color: isColorWhite ? Colors.black : currentColor,
+                      ),
+                      onPressed: () => controller.startColorShuffle(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
