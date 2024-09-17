1ebdf35d476c864c6f5afd60efedf6f596b14fab
romolonobre <romoloferreira@icloud.com>
2024-09-17 09:57:52 +0100

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Color Game',
+      debugShowCheckedModeBanner: false,
       home: ColorGameScreen(),
     );
   }
